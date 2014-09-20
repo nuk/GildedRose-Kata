6842b1f58f5623b1870946d0f6822445bda1a96b
Istvan Demeter <istvan.demeter@emarsys.com>
2014-09-20 17:10:42 +0200

    Create tests for the already handled cases.

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -4,15 +4,17 @@ class GildedRose
 
   @items = []
 
-  def initialize
-    @items = []
-    @items << Item.new("+5 Dexterity Vest", 10, 20)
-    @items << Item.new("Aged Brie", 2, 0)
-    @items << Item.new("Elixir of the Mongoose", 5, 7)
-    @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
-    @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
-    @items << Item.new("Conjured Mana Cake", 3, 6)
+  def initialize(items)
+    @items = Array(items)
+    # @items << Item.new("+5 Dexterity Vest", 10, 20)
+    # @items << Item.new("Aged Brie", 2, 0)
+    # @items << Item.new("Elixir of the Mongoose", 5, 7)
+    # @items << Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
+    # @items << Item.new("Backstage passes to a TAFKAL80ETC concert", 15, 20)
+    # @items << Item.new("Conjured Mana Cake", 3, 6)
   end
+
+
 
   def update_quality
 
